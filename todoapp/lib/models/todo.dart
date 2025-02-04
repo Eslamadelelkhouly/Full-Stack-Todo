24b97b41ce490802be65a3a47843659ee05bbbd1
Eslamadelelkhouly <rodyerak@gmail.com>
2025-02-04 02:23:48 +0200

    fetch data from host

---
+++ todoapp/lib/models/todo.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Todo {
+  int id;
+  String title;
+  String desc;
+  String date;
+  Todo({
+    required this.id,
+    required this.title,
+    required this.desc,
+    required this.date,
+  });
+}
--- todoapp/lib/pages/home_page.dart
+++ todoapp/lib/pages/home_page.dart
@@ -1,12 +1,39 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:todoapp/constant/api.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void fetchData() async {
+    try {
+      http.Response response = await http.get(Uri.parse(api));
+      print(response.body);
+    } catch (e) {
+      print("Error is $e");
+    }
+  }
+
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Center(child: Text('Todo App')),
+      ),
     );
   }
 }
