51bbec17938b0e997fedb6588cd3ff9f78242913
Eslamadelelkhouly <rodyerak@gmail.com>
2025-02-06 00:06:50 +0200

    add function post todo

--- todoapp/lib/pages/home_page.dart
+++ todoapp/lib/pages/home_page.dart
@@ -45,6 +45,34 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  void post_todo({String title = "", String desc = ""}) async {
+    try {
+      http.Response response = await http.post(
+        Uri.parse(api),
+        headers: <String, String>{
+          "Content-Type": "application/json;charset=UTF-8",
+        },
+        body: jsonEncode(
+          <String, dynamic>{
+            "title": title,
+            "desc": desc,
+            "isDone": false,
+          },
+        ),
+      );
+      if (response.statusCode == 201) {
+        setState(() {
+          myTodos = [];
+        });
+        fetchData();
+      } else {
+        print("Something have a wrong");
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   void delete_todo(String id) async {
     try {
       http.Response response = await http.delete(Uri.parse(api + "/" + id));
@@ -73,9 +101,16 @@ class _HomePageState extends State<HomePage> {
           children: [
             PieChart(
               dataMap: {
-                "done": done.toDouble(),
-                "incomplete": (myTodos.length - done).toDouble(),
+                "Done": done.toDouble(),
+                "Incomplete": (myTodos.length - done).toDouble(),
               },
+              legendOptions: LegendOptions(
+                legendTextStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
             ),
             isLoading
                 ? CircularProgressIndicator()
@@ -103,112 +138,128 @@ class _HomePageState extends State<HomePage> {
           color: Colors.pink,
         ),
         onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            isScrollControlled: true,
-            builder: (BuildContext context) {
-              return Container(
-                height: MediaQuery.of(context).size.height * 0.80,
-                color: Colors.white,
-                child: Center(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Text(
-                          'Add Todo',
-                          style: TextStyle(
-                            color: Colors.pink,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.90,
-                        child: TextField(
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                          decoration: InputDecoration(
-                            hintText: 'Title',
-                            hintStyle: TextStyle(
-                              color: Colors.white,
-                            ),
-                            filled: true,
-                            fillColor: Color(0xff001133),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            disabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.90,
-                        child: TextField(
-                          maxLines: 5,
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                          decoration: InputDecoration(
-                            hintText: 'Description',
-                            hintStyle: TextStyle(
-                              color: Colors.white,
-                            ),
-                            filled: true,
-                            fillColor: Color(0xff001133),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            disabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Container(
-                        width: 160,
-                        height: 40,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.pink,
-                            textStyle: TextStyle(color: Colors.white),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            'Add',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 280,
-                      )
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
+          CustomBottomSheet(context);
         },
       ),
+    );
+  }
+
+  Future<dynamic> CustomBottomSheet(BuildContext context) {
+    String title = "";
+    String desc = "";
+    return showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.80,
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    'Add Todo',
+                    style: TextStyle(
+                      color: Colors.pink,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.90,
+                  child: TextField(
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: 'Title',
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                      filled: true,
+                      fillColor: Color(0xff001133),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onSubmitted: (value) {
+                      setState(() {
+                        title = value;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.90,
+                  child: TextField(
+                    maxLines: 5,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: 'Description',
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                      filled: true,
+                      fillColor: Color(0xff001133),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onSubmitted: (value) {
+                      setState(() {
+                        desc = value;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: 160,
+                  height: 40,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.pink,
+                      textStyle: TextStyle(color: Colors.white),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () => post_todo(title: title, desc: desc),
+                    child: Text(
+                      'Add',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 280,
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
